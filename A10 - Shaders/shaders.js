c520b67ca79832d087ba8d988cbaaf8a9713ba51
Diego E. Avila <adiego73@gmail.com>
2019-06-10 12:37:17 +0200

    updating some shaders and uploading last shader

--- A10 - Shaders/shaders.js
+++ A10 - Shaders/shaders.js
@@ -56,12 +56,13 @@ var S1 = `
 // Single point light with decay, Lambert diffuse, Blinn specular, no ambient and no emission
 var S2 = `
 	vec3 light_dir = normalize(LAPos - fs_pos);
+	float decay = pow(LATarget / length(LAPos - fs_pos), LADecay);
+	vec4 point_light_color = LAlightColor * decay;
 	
 	vec3 halfVec = normalize(light_dir + eyedirVec);
-	vec4 blinn_specular = LAlightColor * specularColor * pow(clamp(dot(halfVec, normalVec), 0.0, 1.0), SpecShine);
+	vec4 blinn_specular = point_light_color * specularColor * pow(clamp(dot(halfVec, normalVec), 0.0, 1.0), SpecShine);
 	
-	float decay = pow(LATarget / length(LAPos - fs_pos), LADecay);
-	vec4 lambert_diffuse = LAlightColor * decay * diffColor * clamp(dot(normalVec, light_dir), 0.0, 1.0);
+	vec4 lambert_diffuse = point_light_color * diffColor * clamp(dot(normalVec, light_dir), 0.0, 1.0);
 	
 	out_color = lambert_diffuse + blinn_specular;
 	
@@ -83,16 +84,16 @@ var S3 = `
 var S4 = `
 	float LAC_out = cos(radians(LAConeOut / 2.0));
 	float LAC_in = cos(radians(LAConeOut * LAConeIn / 2.0));
+	float decay = pow(LATarget / length(LAPos - fs_pos), LADecay);
 	
 	// calculate the direction of light with the spot model
 	vec3 spot_light_dir = normalize(LAPos - fs_pos);
-	float spot_light_model = clamp((dot(spot_light_dir, LADir) - LAC_out) / (LAC_in - LAC_out), 0.0, 1.0);
+	vec4 spot_light_color = LAlightColor * decay * clamp((dot(spot_light_dir, LADir) - LAC_out) / (LAC_in - LAC_out), 0.0, 1.0);
 	
 	vec3 halfVec = normalize(spot_light_dir + eyedirVec);
-	vec4 blinn_specular = LAlightColor * specularColor * pow(clamp(dot(halfVec, normalVec), 0.0, 1.0), SpecShine);
+	vec4 blinn_specular = spot_light_color * specularColor * pow(clamp(dot(halfVec, normalVec), 0.0, 1.0), SpecShine);
 
-	float decay = pow(LATarget / length(LAPos - fs_pos), LADecay);
-	vec4 lambert_diffuse = LAlightColor * decay * spot_light_model * diffColor * clamp(dot(normalVec, spot_light_dir), 0.0, 1.0);
+	vec4 lambert_diffuse = spot_light_color * diffColor * clamp(dot(normalVec, spot_light_dir), 0.0, 1.0);
 	
 	out_color = clamp(lambert_diffuse + blinn_specular, 0.0, 1.0);
 `;
@@ -130,8 +131,46 @@ var S6 = `
 
 // Three lights: a directional, a point and a spot. Lambert diffuse, phong specular, constant ambient and no emission
 var S7 = `
-	out_color = vec4(1.0, 1.0, 1.0, 1.0);
+//	LIGHT 1 - DIRECTIONAL
+	vec3 reflection_1 = 2.0 * dot(normalVec, LADir) * normalVec - LADir;
+	vec4 phong_specular_1 = LAlightColor * specularColor * pow(clamp(dot(reflection_1, eyedirVec), 0.0, 1.0), SpecShine);
+	
+	vec4 lambert_diffuse_1 = LAlightColor * diffColor * clamp(dot(normalVec, LADir), 0.0, 1.0);
+	
+	vec4 light_1 = lambert_diffuse_1 + phong_specular_1;
+	
+//	LIGHT 2 - POINT
+	vec3 light_2_dir = normalize(LBPos - fs_pos);
+	float decay_2 = pow(LBTarget / length(LBPos - fs_pos), LBDecay);
+	vec4 point_light_color = LBlightColor * decay_2;
+	
+	vec3 reflection_2 = 2.0 * dot(normalVec, light_2_dir) * normalVec - light_2_dir;
+	vec4 phong_specular_2 = point_light_color * specularColor * pow(clamp(dot(reflection_2, eyedirVec), 0.0, 1.0), SpecShine);
+	
+	vec4 lambert_diffuse_2 = point_light_color * diffColor * clamp(dot(normalVec, light_2_dir), 0.0, 1.0);
+	
+	vec4 light_2 = lambert_diffuse_2 + phong_specular_2;
+
+//	LIGHT 3 - SPOT
+	float LCC_out = cos(radians(LCConeOut / 2.0));
+	float LCC_in = cos(radians(LCConeOut * LCConeIn / 2.0));
+	
+	float decay_3 = pow(LCTarget / length(LCPos - fs_pos), LCDecay);
+	
+	// calculate the direction of light with the spot model
+	vec3 light_3_dir = normalize(LCPos - fs_pos);
+	vec4 spot_light_color = LClightColor * decay_3 * clamp((dot(light_3_dir, LCDir) - LCC_out) / (LCC_in - LCC_out), 0.0, 1.0);
+	
+	vec3 reflection_3 = 2.0 * dot(normalVec, light_3_dir) * normalVec - light_3_dir;
+	vec4 phong_specular_3 = spot_light_color * specularColor * pow(clamp(dot(reflection_3, eyedirVec), 0.0, 1.0), SpecShine);
+
+	vec4 lambert_diffuse_3 = spot_light_color * diffColor * clamp(dot(normalVec, light_3_dir), 0.0, 1.0);
+	
+	vec4 light_3 = lambert_diffuse_3 + phong_specular_3;
+
+	vec4 ambient = ambientLightColor * ambColor;
+
+	out_color = clamp( light_1 + light_2 + light_3 + ambient , 0.0, 1.0);
 `;
 	return [S1, S2, S3, S4, S5, S6, S7];
 }
-
